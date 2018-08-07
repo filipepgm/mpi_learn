febe10702158ccf560bb57af75877eba5e15b5ac
Filipe <filipepgm@hotmail.com>
2018-08-07 17:28:41 +0200

    weights var initialization

--- mpi_learn/train/optimizer.py
+++ mpi_learn/train/optimizer.py
@@ -229,7 +229,7 @@ class Adam(RunningAverageOptimizer):
     def apply_update(self, weights, gradient):
         if self.do_reset:
             self.setup_update_graph(weights)
-            self.sess.run([v.initializer for v in self.running_g2]+[v.initializer for v in self.m])
+            self.sess.run([v.initializer for v in self.running_g2]+[v.initializer for v in self.m]+[v.initializer for v in self.weights])
             self.do_reset = False
         #update vars
 
